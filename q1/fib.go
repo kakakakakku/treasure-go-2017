16d41ded38d4492b197d1c231fbd88b7f3a08c74
kakakakakku <y.yoshida22@gmail.com>
2017-10-12 00:02:28 +0900

    Refactored fib.go

--- q1/fib.go
+++ q1/fib.go
@@ -12,11 +12,8 @@ func fib(n int) int {
 		pp.Printf("%s is unsupported number\n", n)
 		os.Exit(1)
 	}
-	if n == 0 {
-		return 0
-	}
-	if n == 1 {
-		return 1
+	if n == 0 || n == 1 {
+		return n
 	}
 	return fib(n-2) + fib(n-1)
 }
